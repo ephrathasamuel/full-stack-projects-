707d67a7196e97376a2d3a228f8c013553317fbc
ephratha samuel <sephratha@gmail.com>
2026-07-26 18:02:16 +0300

    practice

---
+++ day2/project_exersise.py
@@ -0,0 +1,19 @@
+class Account:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance 
+    def Deposite(self, amount):
+        if amount <= 0:
+            print("Error: Amount must be positive")
+        else:
+         self.balance = self.balance + amount
+    
+            
+    def statement(self):
+        print (f"{self.owner} has {self.balance} ETB")
+acc = Account("selam", 50)
+acc.Deposite(0)
+acc.statement()
+
+               
+                       
