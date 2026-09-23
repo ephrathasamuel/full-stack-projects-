7ac58d658685f2b21f3f126c69fb8110512cb1a8
ephratha samuel <sephratha@gmail.com>
2026-09-23 22:54:53 +0300

    hello repo

--- day2/project_exersise.py
+++ day2/project_exersise.py
@@ -14,8 +14,6 @@ class Account:
 acc = Account("selam", 50)
 acc.Deposite(0)
 acc.statement()
-// testing the wheels 
+'''' testing the wheels'''''
                
-  acc = Account("selam", 50)
-acc.Deposite(0)
-acc.statement()                     
+                  
